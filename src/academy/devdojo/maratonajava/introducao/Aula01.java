d568ca4d673054020e34ddf7637c386baeda82eb
ezbueno <ezandrobueno@yahoo.com.br>
2021-09-24 14:10:00 -0300

    introdução

--- src/academy/devdojo/maratonajava/introducao/Aula01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula01.java
@@ -1,12 +1,9 @@
 package academy.devdojo.maratonajava.introducao;
 
-/**
- * Introdução
- */
-
 public class Aula01 {
 
     public static void main(String[] args) {
         System.out.println("KA-ME-HA-ME-HA");
     }
+
 }
