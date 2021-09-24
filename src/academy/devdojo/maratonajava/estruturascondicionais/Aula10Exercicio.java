359f70fea2be5c603c5c1334487d2cf971a4bd29
ezbueno <ezandrobueno@yahoo.com.br>
2021-09-24 14:02:54 -0300

    switch

---
+++ src/academy/devdojo/maratonajava/estruturascondicionais/Aula10Exercicio.java
@@ -0,0 +1,31 @@
+package academy.devdojo.maratonajava.estruturascondicionais;
+
+public class Aula10Exercicio {
+
+
+    public static void main(String[] args) {
+        int dia = 1;
+
+        switch (dia) {
+            case 1:
+            case 7:
+                System.out.println("Final de semana");
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Dia útil");
+                break;
+            default:
+                System.out.println("Opção inválida");
+        }
+
+        switch (dia) {
+            case 1, 7 -> System.out.println("Final de Semana");
+            case 2, 3, 4, 5, 6 -> System.out.println("Dia útil");
+            default -> System.out.println("Opção inválida");
+        }
+    }
+}
