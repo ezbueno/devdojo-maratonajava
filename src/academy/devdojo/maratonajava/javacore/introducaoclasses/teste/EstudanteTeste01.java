daca7a2685faf153fc3150c8013933de93613d85
ezbueno <ezandrobueno@yahoo.com.br>
2021-09-26 01:29:37 -0300

    introdução-classes

---
+++ src/academy/devdojo/maratonajava/javacore/introducaoclasses/teste/EstudanteTeste01.java
@@ -0,0 +1,18 @@
+package academy.devdojo.maratonajava.javacore.introducaoclasses.teste;
+
+import academy.devdojo.maratonajava.javacore.introducaoclasses.dominio.Estudante;
+
+public class EstudanteTeste01 {
+
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Luffy";
+        estudante.idade = 21;
+        estudante.sexo = 'M';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+    }
+
+}
