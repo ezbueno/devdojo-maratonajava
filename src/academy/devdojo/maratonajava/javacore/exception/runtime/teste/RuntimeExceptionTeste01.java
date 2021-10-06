71e2c503ea28d05d9dbffb52916155598adaa5e0
ezbueno <ezandrobueno@yahoo.com.br>
2021-10-06 20:48:17 -0300

    RuntimeException

---
+++ src/academy/devdojo/maratonajava/javacore/exception/runtime/teste/RuntimeExceptionTeste01.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.exception.runtime.teste;
+
+public class RuntimeExceptionTeste01 {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2};
+        System.out.println(nums[2]);
+    }
+
+}
