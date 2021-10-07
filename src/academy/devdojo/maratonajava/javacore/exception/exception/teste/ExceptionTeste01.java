1b16e82a2d2e39200f36e342ceeccb5f8cb35ad9
ezbueno <ezandrobueno@yahoo.com.br>
2021-10-06 21:08:59 -0300

    Exception

---
+++ src/academy/devdojo/maratonajava/javacore/exception/exception/teste/ExceptionTeste01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.exception.exception.teste;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTeste01 {
+
+    public static void main(String[] args) {
+        criarNovoArquivo();
+    }
+
+    private static void criarNovoArquivo() {
+        File file = new File("arquivo\\teste.txt");
+        try {
+            boolean criado = file.createNewFile();
+            System.out.println("Arquivo criado: " + criado);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
