27305cd279de147c36b32ae759d696031135b017
ezbueno <ezandrobueno@yahoo.com.br>
2021-10-20 20:33:20 -0300

    Multi catch em linha

---
+++ src/academy/devdojo/maratonajava/javacore/exception/runtime/teste/RuntimeExceptionTeste05.java
@@ -0,0 +1,14 @@
+package academy.devdojo.maratonajava.javacore.exception.runtime.teste;
+
+public class RuntimeExceptionTeste05 {
+
+    public static void main(String[] args) {
+        try {
+            throw new ArrayIndexOutOfBoundsException();
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException e) {
+            System.out.println("Dentro do ArrayIndexOutOfBoundsException | IllegalArgumentException | ArithmeticException");
+        } catch (RuntimeException e) {
+            System.out.println("Dentro do RuntimeException");
+        }
+    }
+}
